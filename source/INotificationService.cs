a208744a642c24d0c7fba86f33b77fca01e9bbd7
Benjamin Vertonghen <benjamin.vertonghen@hogent.be>
2021-07-29 12:52:29 +0200

    Permission Status
    - Now it's possible to check the status of the permission.
    - Update packages to latest Blazor version
    Fixes #6

--- source/INotificationService.cs
+++ source/INotificationService.cs
@@ -5,6 +5,12 @@ namespace Append.Blazor.Notifications
     public interface INotificationService
     {
         /// <summary>
+        /// Retrieves the given or denied permission status, default if the permission was not yet requested.
+        /// If you want to request permission, call <seealso cref="RequestPermissionAsync"/>
+        /// </summary>
+        /// <returns><seealso cref="PermissionType"/></returns>
+        PermissionType PermissionStatus { get; }
+        /// <summary>
         /// Checks if the Notifications' API is Support by the browser.
         /// </summary>
         /// <returns></returns>
@@ -16,12 +22,19 @@ namespace Append.Blazor.Notifications
         ValueTask<PermissionType> RequestPermissionAsync();
 
         /// <summary>
-        /// Create a Notification with <seealso cref="NotificationOptions"/>
+        /// Create a Notification with <seealso cref="NotificationOptions"/>.
         /// </summary>
         /// <param name="title"></param>
         /// <param name="options"></param>
         /// <returns></returns>
         ValueTask CreateAsync(string title, NotificationOptions options);
+        /// <summary>
+        /// Creates a Notifcation with the supplied parameters.
+        /// </summary>
+        /// <param name="title">The title of the notification.</param>
+        /// <param name="description">The body or description of the notification.</param>
+        /// <param name="iconUrl">Link to a image, can be remote or served from the filesystem.</param>
+        /// <returns></returns>
         ValueTask CreateAsync(string title, string description, string iconUrl = null);
     }
 }
--- source/NotificationService.cs
+++ source/NotificationService.cs
@@ -6,44 +6,47 @@ namespace Append.Blazor.Notifications
 {
     internal class NotificationService : INotificationService
     {
-        private readonly Lazy<Task<IJSObjectReference>> moduleTask;
-
+        private readonly Lazy<Task<IJSObjectReference>> _moduleTask;
+        /// <inheritdoc/>
+        public PermissionType PermissionStatus { get; private set; }
         public NotificationService(IJSRuntime jsRuntime)
         {
-            moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/Append.Blazor.Notifications/scripts.js").AsTask());
+            _moduleTask = new(() => jsRuntime.InvokeAsync<IJSObjectReference>("import", "./_content/Append.Blazor.Notifications/scripts.js").AsTask());
         }
 
+        /// <inheritdoc/>
         public async ValueTask<bool> IsSupportedByBrowserAsync()
         {
-            var module = await moduleTask.Value;
+            var module = await _moduleTask.Value;
             return await module.InvokeAsync<bool>("isSupported");
         }
 
+        /// <inheritdoc/>
         public async ValueTask<PermissionType> RequestPermissionAsync()
         {
-            var module = await moduleTask.Value;
+            var module = await _moduleTask.Value;
             string permission = await module.InvokeAsync<string>("requestPermission");
 
             if (permission.Equals("granted", StringComparison.InvariantCultureIgnoreCase))
-                return PermissionType.Granted;
+                PermissionStatus = PermissionType.Granted;
 
             if (permission.Equals("denied", StringComparison.InvariantCultureIgnoreCase))
-                return PermissionType.Denied;
+                PermissionStatus = PermissionType.Denied;
 
-            return PermissionType.Default;
+            return PermissionStatus;
         }
 
-
-        public async ValueTask CreateAsync(string title, NotificationOptions options) 
+        /// <inheritdoc/>
+        public async ValueTask CreateAsync(string title, NotificationOptions options)
         {
-            var module = await moduleTask.Value;
+            var module = await _moduleTask.Value;
             await module.InvokeVoidAsync("create", title, options);
         }
-        
 
+        /// <inheritdoc/>
         public async ValueTask CreateAsync(string title, string body, string icon = null)
         {
-            var module = await moduleTask.Value;
+            var module = await _moduleTask.Value;
 
             NotificationOptions options = new NotificationOptions
             {
@@ -56,9 +59,9 @@ namespace Append.Blazor.Notifications
 
         public async ValueTask DisposeAsync()
         {
-            if (moduleTask.IsValueCreated)
+            if (_moduleTask.IsValueCreated)
             {
-                var module = await moduleTask.Value;
+                var module = await _moduleTask.Value;
                 await module.DisposeAsync();
             }
         }
